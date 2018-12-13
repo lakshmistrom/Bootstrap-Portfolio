458739a65437b082e8e0ac28b911713a1dd607df
Lakshmi Strom <lakshmialeman@gmail.com>
2018-12-13 00:30:02 -0800

    modified the navigation bar style for mobile.

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {
-    $("#githubAccount").addClass("border-right");
-    $("#linkedInAccount").addClass("border-right");
+    // $("#githubAccount").addClass("border-right");
+    // $("#linkedInAccount").addClass("border-left");
 });
 function mobileViewUpdate() {
-    if ($(window).width() < 514) {
-        $("#githubAccount").addClass("border-bottom");
-        $("#linkedInAccount").addClass("border-bottom");
-        $("#githubAccount").removeClass("border-right");
-        $("#linkedInAccount").removeClass("border-right");
-    } else {
-        $("#githubAccount").removeClass("border-bottom");
-        $("#linkedInAccount").removeClass("border-bottom");
-        $("#githubAccount").addClass("border-right");
-        $("#linkedInAccount").addClass("border-right");
-    }
+    // if ($(window).width() < 514) {
+    //     $("#githubAccount").addClass("border-bottom");
+    //     $("#linkedInAccount").addClass("border-bottom");
+    //     $("#githubAccount").removeClass("border-left");
+    //     $("#linkedInAccount").removeClass("border-left");
+    // } else {
+    //     $("#githubAccount").removeClass("border-bottom");
+    //     $("#linkedInAccount").removeClass("border-bottom");
+    //     $("#githubAccount").addClass("border-left");
+    //     $("#linkedInAccount").addClass("border-left");
+    // }
 }
 
 $(window).on('load, resize', mobileViewUpdate);
